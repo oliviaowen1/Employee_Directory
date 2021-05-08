a35795c10463d48518f9704b91b845d35b548836
Olivia Owen <olivia.owen@hotmail.com>
2021-05-08 15:59:45 +0100

    Added index and styling for header on page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ const Header = () => (
     <div className="header">
       <h1>Employee Directory</h1>
       <p>
-        Use the search box to find employees by name and click the column headings
+        Please use the search box below to find employees by name or click on the column headings
         to sort.
       </p>
     </div>
