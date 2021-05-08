b53673871254f881ccf702e60aceba6808164824
Olivia Owen <olivia.owen@hotmail.com>
2021-05-08 16:23:14 +0100

    Updated API

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = {
     getUsers: function() {
-        return axios.get("https://randomuser.me/api/?results=200&nat=us");
+        return axios.get("https://randomuser.me/api/?results=100&nat=gb");
     },
 };
 
