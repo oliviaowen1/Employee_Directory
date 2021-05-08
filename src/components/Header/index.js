d003ae8dff88cdc4003e3b897caa57223ce309e3
Olivia Owen <olivia.owen@hotmail.com>
2021-05-08 16:09:18 +0100

    Updated text and styling for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,8 @@ const Header = () => (
     <div className="header">
       <h1>Employee Directory</h1>
       <p>
-        Please use the search box below to find employees by name or click on the column headings
-        to sort.
+          Welcome to Employee Directory! <br></br>
+        Please use the search box below to find employees by name or click on the column headings to sort.
       </p>
     </div>
   );
