a38177e0661a430038643178fc4bcacb06641308
Olivia Owen <olivia.owen@hotmail.com>
2021-05-08 18:03:11 +0100

    Updated app to import main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Main from './components/Main';
 import Wrapper from './components/Wrapper'
 
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Wrapper>
       <Header />
+      <Main />
       </Wrapper>
     </div>
   );
