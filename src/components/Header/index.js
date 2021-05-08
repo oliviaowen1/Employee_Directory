320f7ca9050411ede7670d498b45017fd11bfd95
Olivia Owen <olivia.owen@hotmail.com>
2021-05-08 15:57:41 +0100

    Created components and header file

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const Header = () => (
+    <div className="header">
+      <h1>Employee Directory</h1>
+      <p>
+        Use the search box to find employees by name and click the column headings
+        to sort.
+      </p>
+    </div>
+  );
+  
+  export default Header;
